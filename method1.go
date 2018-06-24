3d27eb57b7cc2aa797af17542faaeeb1e25348ad
wqer <1972324624@qq.com>
2018-06-25 03:17:30 +0800

    update

--- method1.go
+++ method1.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-const(
-	WHITE = iota
+const (
+	WHITE  = iota
 	BLACK
 	BLUE
 	RED
@@ -14,23 +14,27 @@ type Color byte
 
 type Box struct {
 	width, height, depth float64
-	color Color
+	color                Color
 }
 
 type BoxList []Box //a slice of boxes
 
+// 返回 box 的容量
 func (b Box) Volume() float64 {
 	return b.width * b.height * b.depth
 }
 
+// 設置顏色
 func (b *Box) SetColor(c Color) {
 	b.color = c
 }
 
+// 顏色
 func (bl BoxList) BiggestColor() Color {
-	v := 0.00
+	v := bl[0].Volume()
 	k := Color(WHITE)
 	for _, b := range bl {
+		// 查找容量最大的
 		if bv := b.Volume(); bv > v {
 			v = bv
 			k = b.color
@@ -46,28 +50,25 @@ func (bl BoxList) PaintItBlack() {
 }
 
 func (c Color) String() string {
-	strings := []string {"WHITE", "BLACK", "BLUE", "RED", "YELLOW"}
+	strings := []string{"WHITE", "BLACK", "BLUE", "RED", "YELLOW"}
 	return strings[c]
 }
 
 func main() {
-	boxes := BoxList {
-		Box{4, 4, 4, RED},
-		Box{10, 10, 1, YELLOW},
-		Box{1, 1, 20, BLACK},
-		Box{10, 10, 1, BLUE},
-		Box{10, 30, 1, WHITE},
-		Box{20, 20, 20, YELLOW},
+	boxes := BoxList{
+		Box{1, 1, 1, RED},    // 1
+		Box{2, 2, 2, YELLOW}, // 8
+		Box{3, 3, 3, BLACK},  // 27
+		Box{4, 4, 4, BLUE},   // 64
+		Box{5, 5, 5, WHITE},  // 125
 	}
 
 	fmt.Printf("We have %d boxes in our set\n", len(boxes))
 	fmt.Println("The volume of the first one is", boxes[0].Volume(), "cm³")
-	fmt.Println("The color of the last one is",boxes[len(boxes)-1].color.String())
+	fmt.Println("The color of the last one is", boxes[len(boxes)-1].color.String())
 	fmt.Println("The biggest one is", boxes.BiggestColor().String())
-
 	fmt.Println("Let's paint them all black")
 	boxes.PaintItBlack()
 	fmt.Println("The color of the second one is", boxes[1].color.String())
-
 	fmt.Println("Obviously, now, the biggest one is", boxes.BiggestColor().String())
 }
--- type1.go
+++ type1.go
@@ -12,7 +12,16 @@ func main() {
 	m := months{
 		"January":  31,
 		"February": 28,
-		"December":31,
+		"December": 31,
 	}
 	fmt.Println(m)
+
+	var day map[string]int
+	day = make(map[string]int)
+	day["hello"] = 1
+
+	a := map[string]int{
+		"hello": 1,
+	}
+	fmt.Println(a)
 }
