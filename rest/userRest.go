52ec676e66316fe8623c7be406e1b743de2b92b6
wq1019 <2013855675@qq.com>
2018-09-15 21:21:01 +0800

    update

---
+++ rest/userRest.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+func Show(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	userId := ps.ByName("user_id")
+	fmt.Fprintf(w, "you are get user %s", userId)
+}
+func Update(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	userId := ps.ByName("user_id")
+	fmt.Fprintf(w, "you are update user %s", userId)
+}
+func Delete(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	userId := ps.ByName("user_id")
+	fmt.Fprintf(w, "you are delete user %s", userId)
+}
+func Store(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "you are store user.")
+}
+func main() {
+	router := httprouter.New()
+	router.GET("/users/", Index)
+	router.POST("/users/", Store)
+	router.GET("/users/:user_id", Show)
+	router.PUT("/users/:user_id", Update)
+	router.DELETE("/users/:user_id", Delete)
+
+	log.Fatal(http.ListenAndServe(":8088", router))
+}
